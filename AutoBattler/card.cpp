694ae95d368818c39f56a47f72651876475c4a42
CR0ZER <109722353+CR0ZER@users.noreply.github.com>
2022-12-09 10:48:07 +0100

    card class

--- AutoBattler/card.cpp
+++ AutoBattler/card.cpp
@@ -3,7 +3,7 @@
 Card::Card(int p_cardID, std::string p_cardName, int p_life, int p_attack, int p_manaCost, int p_goldCost)
 {
     cardID = p_cardID;
-    cardName = p_cardName
+    cardName = p_cardName;
     life = p_life;
     p_life>0 ? isAlive = true : isAlive = false;
     attack = p_attack;
@@ -17,19 +17,9 @@ Card::~Card()
 
 void Card::useOn(Card card)
 {
-    if(cardID == 0){
-        int choose;
-        printf("Choose a card to use on: \n");
-        scanf("%d", &choose);
-        //TODO: use card on choosen card from enemy boardDeck
-    }
-    else if(cardID == 1){
-        int n=0;
-        int value;
-        while(GetKeyState(VK_ESCAPE) & 0x8000/*TODO || n>=deckBoard <=> si on a choisi tt les cartes adverses*/){
-            printf("Choose a card to use on or press ESCAPE to leave: \n");
-            scanf("%d", &value);
-            Card card_choose = deckBoard[value];
-        }
-    }
+    //TODO
+    //ID 0: Attack card
+    //ID 1: Attack multiple cards
+    //ID 2: Attack player
+    //ID 3: special ID for Hero
 }
--- AutoBattler/deck.h
+++ AutoBattler/deck.h
@@ -1,6 +1,8 @@
 #pragma once
+#include "card.h"
 #include <string>
 #include <vector>
+#include <algorithm>
 
 class deck
 {
